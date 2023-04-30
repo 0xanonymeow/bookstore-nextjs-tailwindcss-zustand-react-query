2f1bf321cb731a0308ba8cafe822fe4c4aa8a113
0xanonymeow <anonymeow@incatni.to>
2023-04-30 22:03:10 +0700

    style: update banner and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link'
 import { buttonVariants } from '@/components/Button'
 import ThemeToggle from '@/components/ThemeToggle'
+import { cn } from '@/lib/utils'
 
 const Navbar = ({}) => {
   return (
     <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between transition-colors duration-500">
       <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
-        <Link href="/" className={buttonVariants({ variant: 'link' })}>
+        <Link
+          href="/"
+          className={cn(buttonVariants({ variant: 'link' }), 'sm:text-xl ')}
+        >
           Bedtime Story
         </Link>
         <div className="md:hidden">
@@ -16,22 +20,25 @@ const Navbar = ({}) => {
           <ThemeToggle />
           <Link
             href="/explore"
-            className={buttonVariants({ variant: 'ghost' })}
+            className={cn(buttonVariants({ variant: 'ghost' }), 'sm: text-lg')}
           >
             Explore
           </Link>
           <Link
             href="/categories"
-            className={buttonVariants({ variant: 'ghost' })}
+            className={cn(buttonVariants({ variant: 'ghost' }), 'sm: text-lg')}
           >
             Bestseller
           </Link>
-          <Link href="/saved" className={buttonVariants({ variant: 'ghost' })}>
+          <Link
+            href="/saved"
+            className={cn(buttonVariants({ variant: 'ghost' }), 'sm: text-lg')}
+          >
             Category
           </Link>
           <Link
             href="/categories"
-            className={buttonVariants({ variant: 'ghost' })}
+            className={cn(buttonVariants({ variant: 'ghost' }), 'sm: text-lg')}
           >
             Saved
           </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import LargeHeading from '@/components/LargeHeading'
 import Paragraph from '@/components/Paragraph'
 import type { Metadata } from 'next'
+import { useTheme } from 'next-themes'
 
 export const metadata: Metadata = {
   title: '',
@@ -8,12 +9,16 @@ export const metadata: Metadata = {
 }
 
 export default function Page() {
+  const { theme } = useTheme()
+
   return (
     <>
       <div
         className="absolute top-0 w-full h-1/2 xl:h-2/3"
         style={{
-          backgroundImage: `url(/banner.png)`,
+          backgroundImage: `linear-gradient(to bottom, transparent 80%, ${
+            theme === 'dark' ? 'rgb(15 23 42)' : 'rgb(248 250 252)'
+          }100%), url("/banner.png")`,
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat',
         }}
@@ -23,11 +28,11 @@ export default function Page() {
           <div className="h-full gap-6 flex flex-col justify-start xl:justify-center items-center lg:items-start">
             <LargeHeading
               size="lg"
-              className="three-d text-light-gold dark:text-white backdrop-blur-sm"
+              className="three-d text-light-gold dark:text-white "
             >
               Cozy Bedtime Routine for Little Ones
             </LargeHeading>
-            <Paragraph className="p-three-d text-white dark:text-white lg:text-left backdrop-blur-sm">
+            <Paragraph className="p-three-d text-white dark:text-white lg:text-left">
               Create a Soothing Bedtime Environment with Our Handpicked Books
             </Paragraph>
             <div className="relative w-full max-w-lg lg:max-w-3xl lg:left-1/2 aspect-[16/9] lg:absolute"></div>
