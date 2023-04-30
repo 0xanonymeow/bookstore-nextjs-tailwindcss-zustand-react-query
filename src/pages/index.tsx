b3dc84408d221ef354b7e0dcddeb7c02edf39b4e
0xanonymeow <anonymeow@incatni.to>
2023-04-30 21:16:24 +0700

    feat: update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,39 @@
-import { Inter } from 'next/font/google'
+import LargeHeading from '@/components/LargeHeading'
+import Paragraph from '@/components/Paragraph'
+import type { Metadata } from 'next'
 
-const inter = Inter({ subsets: ['latin'] })
+export const metadata: Metadata = {
+  title: '',
+  description: '',
+}
 
-export default function Home() {
-  return <></>
+export default function Page() {
+  return (
+    <>
+      <div
+        className="absolute w-full h-1/2 top-0"
+        style={{
+          backgroundImage: `url(/banner.png)`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      />
+      <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+        <div className="container pt-32 max-w-7xl mx-auto w-full h-full">
+          <div className="h-full gap-6 flex flex-col justify-start xl:justify-center items-center lg:items-start">
+            <LargeHeading
+              size="lg"
+              className="three-d text-light-gold dark:text-white backdrop-blur-"
+            >
+              Cozy Bedtime Routine for Little Ones
+            </LargeHeading>
+            <Paragraph className="p-three-d text-white dark:text-white lg:text-left backdrop-blur-sm">
+              Create a Soothing Bedtime Environment with Our Handpicked Books
+            </Paragraph>
+            <div className="relative w-full max-w-lg lg:max-w-3xl lg:left-1/2 aspect-[16/9] lg:absolute"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
